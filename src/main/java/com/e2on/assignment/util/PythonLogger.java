0aa8b9f5c693fd4341f00584427c9a04dd6b284f
songyi <song21677@naver.com>
2024-08-02 10:10:20 +0900

    feat: python 로그에서 x, y, w, h, confidene, cls 추출

--- src/main/java/com/e2on/assignment/util/PythonLogger.java
+++ src/main/java/com/e2on/assignment/util/PythonLogger.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PythonLogger {
 
@@ -14,6 +16,15 @@ public class PythonLogger {
         String line;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
+
+            String regex ="xywhn\tclass\tconfidence\t([0-9.\t]+)";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(line);
+
+            if (matcher.find()) {
+                String group = matcher.group(1);
+                String[] result = group.split("\t");
+            }
         }
 
         process.waitFor();
